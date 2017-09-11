93a6a9b79549c5d53c752b7f0418f37795ef660e
thomasvolz <thomasvolz@u.boisestate.edu>
2017-09-10 19:27:40 -0600

    Code Cleanup and some testing

--- WebContent/js/controllers/MainController.js
+++ WebContent/js/controllers/MainController.js
@@ -2,7 +2,10 @@ var api_key='bfabcf706e4bf2736881f5765f61c5dd';
 var movie='Deadpool';
 
 app.controller('MainController', ['$scope', function($scope, $http) { 
-	$scope.movies = $http.get('https://api.themoviedb.org/3/search/movie?api_key='+api_key+'&language=en-US&query='+movie+'&page=1&include_adult=false');
+	$http.get('https://api.themoviedb.org/3/search/movie?api_key='+api_key+'&language=en-US&query='+movie+'&page=1&include_adult=false'.then(function(response){
+	$scope.movieList = response.data;	
+	}));
+	
 }]);
 
 
