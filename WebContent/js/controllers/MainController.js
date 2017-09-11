b03880de4798bd80061c49e138979b2aa227256b
thomasvolz <thomasvolz@u.boisestate.edu>
2017-09-10 19:27:56 -0600

    Cleanup

--- WebContent/js/controllers/MainController.js
+++ WebContent/js/controllers/MainController.js
@@ -7,29 +7,3 @@ app.controller('MainController', ['$scope', function($scope, $http) {
 	}));
 	
 }]);
-
-
-
-//Old example code stuff
-//$scope.title = 'This Month\'s Bestsellers'; 
-//$scope.promo = 'The most popular books this month.';
-//$scope.products = [
-//	  	{ 
-//	    	name: 'The Book of Trees', 
-//	    	price: 19, 
-//	    	pubdate: new Date('2014', '03', '08'), 
-//	    	cover: 'img/the-book-of-trees.jpg' 
-//	  	}, 
-//	  	{ 
-//	    	name: 'Program or be Programmed', 
-//	    	price: 8, 
-//	    	pubdate: new Date('2013', '08', '01'), 
-//	    	cover: 'img/program-or-be-programmed.jpg' 
-//	  	}
-//	  ];
-//$scope.plusOne = function(index) { 
-//	  	$scope.products[index].likes += 1; 
-//		};
-//		$scope.minusOne = function(index) { 
-//	  	$scope.products[index].dislikes += 1; 
-//		};
