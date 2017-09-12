18953f021f7a726b98c14ac17d8775d883f6c5e7
thomasvolz <thomasvolz@u.boisestate.edu>
2017-09-12 13:18:52 -0600

    Basic function working.

--- WebContent/js/controllers/MainController.js
+++ WebContent/js/controllers/MainController.js
@@ -1,9 +1,22 @@
 var api_key='bfabcf706e4bf2736881f5765f61c5dd';
 var movie='Deadpool';
+var trumovie='Titanic';
+var resultCount;
+app.controller('MainController', ['$scope', '$http', function($scope, $http) { 
 
-app.controller('MainController', ['$scope', function($scope, $http) { 
-	$http.get('https://api.themoviedb.org/3/search/movie?api_key='+api_key+'&language=en-US&query='+movie+'&page=1&include_adult=false'.then(function(response){
-	$scope.movieList = response.data;	
-	}));
-	
+	$scope.submitFunction = function(){
+	    $http.get('https://api.themoviedb.org/3/search/movie?api_key='+api_key+'&language=en-US&query='+$scope.movieTitle+'&page=1&include_adult=false')
+	        .then(successCallback, errorCallback);
+	}
+	function successCallback(response){
+		//$scope.resultList = response.data
+		$scope.results = response.data.total_results
+		$scope.movieList = response.data.results
+		
+	}
+
+	function errorCallback(error){
+	}
+
 }]);
+
