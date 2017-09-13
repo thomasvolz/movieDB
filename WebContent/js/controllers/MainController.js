a46b1aa534216a3ecadac713c26569e93a0b17bd
thomasvolz <thomasvolz@u.boisestate.edu>
2017-09-13 15:58:22 -0600

    Completed initial functional build

--- WebContent/js/controllers/MainController.js
+++ WebContent/js/controllers/MainController.js
@@ -2,8 +2,14 @@ var api_key='bfabcf706e4bf2736881f5765f61c5dd';
 var movie='Deadpool';
 var trumovie='Titanic';
 var resultCount;
-app.controller('MainController', ['$scope', '$http', function($scope, $http) { 
 
+/*Main controller function
+ * Makes a 'get' request to the moviedb api using my api key
+ * with a query sent from the submit form from the index.html page
+ * returns a data set that contains an array of movie information
+ * The movie array is accessed in response.data.results
+ */
+app.controller('MainController', ['$scope', '$http', function($scope, $http) { 
 	$scope.submitFunction = function(){
 	    $http.get('https://api.themoviedb.org/3/search/movie?api_key='+api_key+'&language=en-US&query='+$scope.movieTitle+'&page=1&include_adult=false')
 	        .then(successCallback, errorCallback);
